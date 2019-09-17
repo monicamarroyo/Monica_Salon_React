9eea7ce056b59614fba9e1db8b7a94aa2ca0fc60
monica <monica.arroyo1319@gmail.com>
2019-09-16 23:25:33 -0500

    Updates

---
+++ precache-manifest.a0760f044765eb151b66e0e46f2cf9b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29620539d2e04da4fc56d48f02b65fb6",
+    "url": "/monicamarroyo/Monica_Salon_React/index.html"
+  },
+  {
+    "revision": "6e67a641d18de1cea672",
+    "url": "/monicamarroyo/Monica_Salon_React/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "745565be274419af5536",
+    "url": "/monicamarroyo/Monica_Salon_React/static/css/main.0e63b2fe.chunk.css"
+  },
+  {
+    "revision": "6e67a641d18de1cea672",
+    "url": "/monicamarroyo/Monica_Salon_React/static/js/2.8e076ab1.chunk.js"
+  },
+  {
+    "revision": "745565be274419af5536",
+    "url": "/monicamarroyo/Monica_Salon_React/static/js/main.c3dd96d1.chunk.js"
+  },
+  {
+    "revision": "e3384164a5f997ee8f0d",
+    "url": "/monicamarroyo/Monica_Salon_React/static/js/runtime~main.92a4114b.js"
+  },
+  {
+    "revision": "f7fe712ce0c18b73fe56ed00a08b92de",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/brick2.f7fe712c.jpg"
+  },
+  {
+    "revision": "608c3e129d13b44c0e29af3fffab8db4",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/building.608c3e12.jpg"
+  },
+  {
+    "revision": "52866f1e2d6cfc2ea8caf2b643befb20",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/stylist1.52866f1e.jpg"
+  },
+  {
+    "revision": "7631af408bbee3478b7b0851b15197f9",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/stylist2.7631af40.jpg"
+  },
+  {
+    "revision": "b566eb5c17abce7c9af39cc98f0a9cc3",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/stylist4.b566eb5c.jpg"
+  },
+  {
+    "revision": "1b5b2595cbf19b1d3363ecc964430b2c",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/stylist5.1b5b2595.jpg"
+  },
+  {
+    "revision": "3d4b888e981c597722de7c412e11e16e",
+    "url": "/monicamarroyo/Monica_Salon_React/static/media/stylist6.3d4b888e.jpg"
+  }
+]);
